55a335110795b03345a7a13c18f3e9fcaf605cf0
Manish <manishguhe301@gmail.com>
2024-12-09 13:15:25 +0530

    feat: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <h1>Frontend Assignment</h1>
-            {/* TODO: Add dropdown and table components here */}
-        </div>
-    );
+  return (
+    <div>
+      <h1>Frontend Assignment</h1>
+      {/* TODO: Add dropdown and table components here */}
+    </div>
+  );
 };
 
 export default App;
